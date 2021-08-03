7d420cad54ac53947f687abd18c91178c3a812ec
MagdaBer <magda.berger@gmx.de>
2021-08-03 14:42:22 +0200

    🎨  Use the createElement function to recreate main, title and input

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const mainElement = document.createElement('main');
-const title = document.createElement('h1');
-title.innerText = 'Rick and Morty';
-title.className = 'app__title';
-
-const input = document.createElement('input');
-input.placeholder = 'Search for a character...';
-input.className = 'app__userInput';
-
-mainElement.append(title, input);
-
+const mainElement = createElement('main', {
+  childElements: [
+    createElement('h1', {
+      innerText: 'Rick and Morty',
+      className: 'app__title',
+    }),
+    createElement('input', {
+      placeholder: 'Search for a character...',
+      className: 'app__userInput',
+    }),
+  ],
+});
 if (app !== null) {
   app.append(mainElement);
 }
