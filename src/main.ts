0ee50bdca3d917eed92d86cc0f2b16b0acb58efc
MagdaBer <magda.berger@gmx.de>
2021-08-03 13:27:21 +0200

    ✨  Add title element

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import './style.css';
 const app = document.querySelector<HTMLDivElement>('#app');
 
 const mainElement = document.createElement('main');
+const title = document.createElement('h1');
+title.innerText = 'Rick and Morty';
+
+mainElement.append(title);
 
 if (app !== null) {
   app.append(mainElement);
