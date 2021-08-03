60bfdcc5ccb6f84a5c692909e5fe734fd4daf4bb
MagdaBer <magda.berger@gmx.de>
2021-08-03 13:42:20 +0200

    ✨  Add class to title

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const app = document.querySelector<HTMLDivElement>('#app');
 const mainElement = document.createElement('main');
 const title = document.createElement('h1');
 title.innerText = 'Rick and Morty';
+title.className = 'app__title';
 
 const input = document.createElement('input');
 input.placeholder = 'Search for a character...';
