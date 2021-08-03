05e0e46a70ef4bcbf6140766151890c19aabfb6a
MagdaBer <magda.berger@gmx.de>
2021-08-03 18:52:07 +0200

    ✨ Create createCharacterCard function

--- src/components/character/character.ts
+++ src/components/character/character.ts
@@ -1,1 +1,43 @@
 import { createElement } from '../../utils/createElement';
+
+export function createCharacterCard(): HTMLElement {
+  return createElement('article', {
+    className: 'character-card',
+    childElements: [
+      createElement('img', {
+        className: 'character-card__image',
+        src: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      }),
+      createElement('section', {
+        className: 'character-card__info',
+        childElements: [
+          createElement('h2', {
+            className: 'character-card__name',
+            innerText: 'Rick Sanchez',
+          }),
+          createElement('div', {
+            childElements: [
+              createElement('p', {
+                className: 'character-card__status',
+                innerText: '🟢 Alive',
+              }),
+              createElement('p', {
+                className: 'character-card__species',
+                innerText: 'Human',
+              }),
+            ],
+          }),
+          createElement('div', {
+            childElements: [
+              createElement('h3', { innerText: 'Last known location' }),
+              createElement('p', {
+                className: 'character-card__location',
+                innerText: 'Story Train',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+  });
+}
