93cd7487dfe60310ef3e5e2528f0004c562c05c4
MagdaBer <magda.berger@gmx.de>
2021-08-05 16:21:11 +0200

    ✨ Add API

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,19 @@
-export async function getCharacters() {
+import type { AllCharactersFromAPI, Character } from '../types';
+
+export async function getCharacters(): Promise<Character[]> {
   const response = await fetch('https://rickandmortyapi.com/api/character');
-  const data = await response.json();
+  const data: AllCharactersFromAPI = await response.json();
   const characters = data.results;
-  return characters;
+  const formattedCharacters: Character[] = characters.map((character) => {
+    const formattedCharacter: Character = {
+      name: character.name,
+      status: character.status,
+      species: character.species,
+      location: character.location.name,
+      image: character.image,
+    };
+    return formattedCharacter;
+  });
+
+  return formattedCharacters;
 }
