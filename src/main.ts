43657a0354d3c22b9e6c9273fd028530724c34d8
MagdaBer <magda.berger@gmx.de>
2021-08-09 17:14:20 +0200

    🚑️ Fix top level await

--- src/main.ts
+++ src/main.ts
@@ -4,35 +4,40 @@ import type { Character } from './types';
 import { createElement } from './utils/createElement';
 import { getCharacters } from './utils/api';
 
-const characters: Character[] = await getCharacters();
+async function run() {
+  const characters: Character[] = await getCharacters();
 
-const characterContainer = createElement('div', {
-  className: 'app__characterContainer',
-  childElements: characters.map((character) => createCharacterCard(character)),
-});
+  const characterContainer = createElement('div', {
+    className: 'app__characterContainer',
+    childElements: characters.map((character) =>
+      createCharacterCard(character)
+    ),
+  });
 
-const searchbar = createElement('input', {
-  className: 'app__userInput',
-  placeholder: 'Search for a character',
-  oninput: async () => {
-    characterContainer.innerHTML = '';
-    const search = searchbar.value;
-    const filteredCharacters = await getCharacters(search);
-    const filteredCharacterElements = filteredCharacters.map(
-      (filteredCharacter) => createCharacterCard(filteredCharacter)
-    );
-    characterContainer.append(...filteredCharacterElements);
-  },
-});
+  const searchbar = createElement('input', {
+    className: 'app__userInput',
+    placeholder: 'Search for a character',
+    oninput: async () => {
+      characterContainer.innerHTML = '';
+      const search = searchbar.value;
+      const filteredCharacters = await getCharacters(search);
+      const filteredCharacterElements = filteredCharacters.map(
+        (filteredCharacter) => createCharacterCard(filteredCharacter)
+      );
+      characterContainer.append(...filteredCharacterElements);
+    },
+  });
 
-const mainElement = createElement('main', {
-  childElements: [
-    createElement('h1', {
-      innerText: 'Rick and Morty',
-      className: 'app__title',
-    }),
-    searchbar,
-    characterContainer,
-  ],
-});
-document.querySelector<HTMLDivElement>('#app')?.append(mainElement);
+  const mainElement = createElement('main', {
+    childElements: [
+      createElement('h1', {
+        innerText: 'Rick and Morty',
+        className: 'app__title',
+      }),
+      searchbar,
+      characterContainer,
+    ],
+  });
+  document.querySelector<HTMLDivElement>('#app')?.append(mainElement);
+}
+run();
